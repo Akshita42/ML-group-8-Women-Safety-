90823451bde4971a036e202455f8c1b9734f4867
Eshita Aggarwal <152268046+Eshitaa01@users.noreply.github.com>
2025-04-04 10:56:55 +0530

    Update shake_detection_CNN_LSTM.py

--- shake_detection_CNN_LSTM.py
+++ shake_detection_CNN_LSTM.py
@@ -1,18 +1,18 @@
-import torch.nn as nn
+import torch.nn as nn # pytorch library for deep learning models nn contains multiple layers like lstm
 import torch
 
-class CNN_LSTM(nn.Module):
-    def __init__(self):
-        super(CNN_LSTM, self).__init__()
+class CNN_LSTM(nn.Module):  # inherits from nn module
+    def __init__(self): # defne the layers
+        super(CNN_LSTM, self).__init__() # allows access to methods of parent class
         # 1D convolution layer: input channels = 4, output channels = 32, kernel size = 3, padding = 1
         self.conv1 = nn.Conv1d(in_channels=4, out_channels=32, kernel_size=3, padding=1)
-        self.relu = nn.ReLU()
+        self.relu = nn.ReLU() #  rectified linear function to add non-linerarity and replaces negative value with zero
         self.lstm = nn.LSTM(input_size=32, hidden_size=64, num_layers=2, batch_first=True)
-        self.dropout = nn.Dropout(0.3)
+        self.dropout = nn.Dropout(0.3)# dropping 30% of neurons to prevent overfitting
         self.fc = nn.Linear(64, 1)  # 64 output from LSTM (hidden size)
-        self.sigmoid = nn.Sigmoid()
+        self.sigmoid = nn.Sigmoid() # output  in binary  
 
-    def forward(self, x):
+    def forward(self, x): # this function shows how the data flows throughout the model 
         # Reshape input to (batch_size, channels, sequence_length) if it's not already
         x = x.unsqueeze(2)  # Add the sequence dimension: (batch_size, features, sequence_length)
         
